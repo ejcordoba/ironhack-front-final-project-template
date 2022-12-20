060c3d0fd46a1c9d5fcf1c2ee05d308181826dd7
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2022-12-20 19:38:12 +0100

    Initial setup: main pages, routes and .env data for supabase

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
-
-const routes = [];
+import Auth from '../pages/Auth.vue'
+import Dashboard from '../pages/Dashboard.vue'
+const routes = [
+  { path: '/', component: Dashboard },
+  { path: '/auth', component: Auth },];
+    
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_APP_ENV),
