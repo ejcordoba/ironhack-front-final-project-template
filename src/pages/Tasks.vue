465f500d32b7ae9695d8f9303ad3947f8cb3ff36
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2022-12-31 07:34:38 +0100

    changed app name

--- src/pages/Tasks.vue
+++ src/pages/Tasks.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="p-8 flex items-start bg-light-grey rounded-md shadow-lg">
       <form class="flex flex-col gap-y-5 w-full">
-        <h1 class="text-2xl text-at-light-green">Tasks</h1>
+        <h1 class="text-2xl text-at-light-green">Create a Task</h1>
         <div class="flex flex-col">
           <label for="task-title" class="mb-1 text-sm text-at-light-green"
             >Task title</label
@@ -22,6 +22,23 @@
             v-model="taskTitle"
           />
         </div>
+        <div class="flex flex-col">
+          <label for="task-title" class="mb-1 text-sm text-at-light-green"
+            >Task complete?</label
+          >
+          <input
+            type="checkbox"
+            v-model="taskComplete"
+            true-value="yes"
+            false-value="no"
+          />
+        </div>
+        <button
+          type="button"
+          class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
+        >
+          Add task
+        </button>
       </form>
     </div>
   </div>
@@ -33,6 +50,7 @@ import { useTaskStore } from "../store/task.js";
 
 const tasksArray = ref([]);
 const taskTitle = ref("");
+const taskComplete = ref(false);
 const statusMsg = ref(null);
 const errorMsg = ref(null);
 </script>
