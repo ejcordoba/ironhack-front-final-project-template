a776c9d7cc0ec455c239415ffe9c1cf1e0a82a9b
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2023-01-26 18:19:26 +0100

    update supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://vxjnhlojtcxucczcnkpd.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4am5obG9qdGN4dWNjemNua3BkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE1NTk1NTUsImV4cCI6MTk4NzEzNTU1NX0.lUvP-nqtQUd0s-hP_9KzAsZkT8dYw1RUg4eUM4WHQ7o";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
