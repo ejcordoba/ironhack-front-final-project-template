ca1a9bc9c4e0e7e07cf1bc437fdc8966d93965a6
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2023-01-29 10:18:58 +0100

    Add new style to task creator

--- src/components/TaskCreator.vue
+++ src/components/TaskCreator.vue
@@ -7,30 +7,34 @@
       <p class="text-at-light-green">{{ statusMsg }}</p>
       <p class="text-red-500">{{ errorMsg }}</p>
     </div>
-    <div class="p-8 flex items-start bg-light-grey rounded-md shadow-lg">
-      <form @submit.prevent="addTask" class="flex flex-col gap-y-5 w-full">
-        <h1 class="text-2xl text-at-light-green">Create a Task</h1>
-        <div class="flex flex-col">
-          <label for="task-title" class="mb-1 text-sm text-at-light-green"
-            >Task title</label
-          >
-          <input
-            type="text"
-            required
-            class="p-2 text-gray-500 focus:outline-none"
-            id="task-title"
-            v-model="taskTitle"
-          />
-        </div>
+    <div class="relative">
+      <div
+        class="absolute -inset-0.5 bg-gradient-to-r from-teal-500 via-white to-yellow-400 blur opacity-50 hover:opacity-100 transition duration-200"
+      ></div>
+      <div class="relative p-8 flex items-start bg-white rounded-md shadow-lg">
+        <form @submit.prevent="addTask" class="flex flex-col gap-y-5 w-full">
+          <h1 class="text-2xl text-at-light-green">Create a Task</h1>
+          <div class="flex flex-col">
+            <label for="task-title" class="mb-1 text-sm text-at-light-green"
+              >Task title</label
+            >
+            <input
+              type="text"
+              required
+              class="p-2 text-gray-500 focus:outline-none"
+              id="task-title"
+              v-model="taskTitle"
+            />
+          </div>
 
-        <button
-          type="submit"
-          class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
-        >
-          Add task
-        </button>
-        <hr />
-      </form>
+          <button
+            type="submit"
+            class="mt-6 py-2 px-6 rounded-sm self-start text-sm text-white bg-at-light-green duration-200 border-solid border-2 border-transparent hover:border-at-light-green hover:bg-white hover:text-at-light-green"
+          >
+            Add task
+          </button>
+        </form>
+      </div>
     </div>
   </div>
 </template>
